0728a7c1f741d67b00c6f4847ba26f07a92d23e7
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-06 08:46:05 -0300

    change api request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
+import md5 from "md5";
+
+const ts = Math.floor(Date.now());
+const publicKey = 'e805fe9719d145ceca74a945af62118b';
+const privateKey = 'f6b59c078e44feba8bd7ead221ac82a8bb5f36a1';
+const hash = md5(ts + privateKey + publicKey);
 
 export const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'http://gateway.marvel.com/v1/public/',
+  params: {
+    ts,
+    apikey: publicKey,
+    hash
+  }
 });
+
+export default api;
