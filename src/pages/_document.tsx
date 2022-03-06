5be26362acf350587ee233c9096890c6ca55c36d
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-05 23:31:28 -0300

    change favicon and remove title fixed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,8 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet" />
 
-          <link rel="shortcut icon" href="favicon.ico" type="image/ico" />
-          <title>Marvel Challenge | Magalu</title>
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
         </Head>
 
         <body>
