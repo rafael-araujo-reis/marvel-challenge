ac6e164c2c8d89dec904d3c7691973c1b34a863f
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-06 22:28:17 -0300

    definition card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,8 +7,14 @@ interface CardProps {
 
 export function Card({ title, image }: CardProps) {
   return (
-    <div className={styles.cardContainer}>
-
+    <div className={styles.cardContent}>
+      <div className={styles.backdrop}
+        style={{ backgroundImage: image }}
+      >
+      </div>
+      <div className={styles.titleCard}>
+        <p>{title}</p>
+      </div>
     </div>
   );
 }
