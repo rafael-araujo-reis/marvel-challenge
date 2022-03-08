4b5c22d144486912cd7bc1af32ca50182a92d8e8
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-07 21:52:16 -0300

    add components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Button } from '../components/Button';
 import { Card } from '../components/Card';
 import { Header } from '../components/Header';
+import { Search } from '../components/Search';
 import { useHeroes } from '../hooks/useHeroes';
 import styles from './home.module.scss';
 
@@ -20,6 +21,10 @@ export default function Home() {
 
   const { handleMoreHeroes, heroes } = useHeroes();
 
+  function heroSearch() {
+    console.log('pesquisar');
+  }
+
   return (
     <>
       <Header />
@@ -27,16 +32,26 @@ export default function Home() {
         <title>Marvel Challenge | Magalu</title>
       </Head>
       <main className={styles.homeContainer}>
+
+        <Search
+          placeholder={'Procure por Avangers, Spider-man, Balder ...'}
+        />
+        <Button
+          titleButton={'Pesquisar'}
+          colorTitle={'#FFFFFF'}
+          bgColor={'#700611'}
+          onClick={heroSearch} />
+
         <ul className={styles.cardsContainer}>
           {
             heroes.map((hero) => {
               return (
-                <li key={hero.id}>
-                  <Link href={`/heroDetails/${hero.id}`}>
+                <li key={hero.hero.id}>
+                  <Link href={`/heroDetails/${hero.hero.id}`}>
                     <a>
                       <Card
-                        title={hero.name}
-                        image={`url(${hero.thumbnail.path}.${hero.thumbnail.extension})`}
+                        title={hero.hero.name}
+                        image={`url(${hero.hero.thumbnail.path}.${hero.hero.thumbnail.extension})`}
                       />
                     </a>
                   </Link>
