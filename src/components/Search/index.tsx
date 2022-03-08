f9277ac700458e2f0aaab505dcba31dd8d2a9eaf
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-08 18:41:17 -0300

    styled search component

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,11 +1,30 @@
+import { Button } from '../Button';
 import styles from './styles.module.scss';
 
 interface SearchProps {
   placeholder?: string;
+  typeInput: string,
+
+  titleButton: string,
+  colorTitle: string,
+  bgColor: string,
+  onClick: () => void,
 }
 
-export function Search({ placeholder }: SearchProps) {
+export function Search({ placeholder, typeInput, titleButton, colorTitle, bgColor, onClick }: SearchProps) {
   return (
-    <input placeholder={placeholder} className={styles.searchContainer} />
+    <form className={styles.searchContainer} >
+      <input
+        placeholder={placeholder}
+        className={styles.inputSearch}
+        type={typeInput}
+      />
+      <Button
+        titleButton={titleButton}
+        colorTitle={colorTitle}
+        bgColor={bgColor}
+        onClick={() => onClick}
+        type={'submit'} />
+    </form>
   );
 }
