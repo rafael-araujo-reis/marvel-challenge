f712df8861d46a537373f7dfb1c8f1e8dd19904c
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-08 18:39:59 -0300

    adjust styling

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,14 +5,16 @@ interface ButtonProps {
   colorTitle: string;
   bgColor: string;
   icon?: string;
+  type?: 'button' | 'submit' | 'reset';
   onClick: () => void;
 }
-export function Button({ titleButton, colorTitle, bgColor, onClick }: ButtonProps) {
+export function Button({ titleButton, colorTitle, bgColor, onClick, type }: ButtonProps) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: colorTitle }}
       className={styles.buttonContainer}
       onClick={onClick}
+      type={type}
     >
       {titleButton}
     </button>
