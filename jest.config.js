a5d58a37ef99467f15c099edf3bcf0dbcd579983
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-10 13:06:18 -0300

    configure teste jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
-module.exports = {
-  setupFilesAfterEnv: ["./jest.setup.js"],
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    "^@components(.*)$": "<rootDir>/components$1",
-    "^@pages(.*)$": "<rootDir>/pages$1",
-    "^@hooks(.*)$": "<rootDir>/hooks$1",
-  }
+    '@components/(.*)$': '<rootDir>/components/$1',
+    '@pages/(.*)$': '<rootDir>/pages/$1'
+  },
+  testEnvironment: 'jest-environment-jsdom'
 };
+
+module.exports = createJestConfig(customJestConfig);
