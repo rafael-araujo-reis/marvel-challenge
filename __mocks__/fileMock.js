2099d3d396a283719456d0d35db7796b7508dfd6
Rafael Reis <rafa.araujoreis@gmail.com>
2022-03-11 21:07:00 -0300

    add mocks

--- __mocks__/fileMock.js
+++ __mocks__/fileMock.js
@@ -20,9 +20,52 @@ export const mockHeroData = {
   name: "A-Bomb (HAS)",
   description: "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",
   modified: "2013-09-18T15:54:04-0400",
+  favorite: true,
   thumbnail: {
     path: "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16",
     extension: "jpg"
   },
   resourceURI: "http://gateway.marvel.com/v1/public/characters/1017100"
+};
+
+export const mockHeroDetailData = {
+  id: 1017100,
+  name: "A-Bomb (HAS)",
+  description: "Rick Jones has been Hulk's best bud since day one, but now he's more than a friend...he's a teammate! Transformed by a Gamma energy explosion, A-Bomb's thick, armored skin is just as strong and powerful as it is blue. And when he curls into action, he uses it like a giant bowling ball of destruction! ",
+  modified: "2013-09-18T15:54:04-0400",
+  thumbnail: {
+    path: "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16",
+    extension: "jpg"
+  },
+  resourceURI: 'http://',
+  comics: {
+    available: 10,
+    collectionURI: 'http://',
+    items: [],
+    returned: 0
+  },
+  series: {
+    available: 10,
+    collectionURI: 'http://',
+    items: [],
+    returned: 0
+  },
+  stories: {
+    available: 10,
+    collectionURI: 'http://',
+    items: [],
+    returned: 0
+  },
+  events: {
+    available: 10,
+    collectionURI: 'http://',
+    items: [],
+    returned: 0
+  },
+  urls: [
+    {
+      type: 'type',
+      url: 'http://'
+    }
+  ]
 };
